441239546886de4ae856e8bcd680c371d4e8801d
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2024-07-12 16:49:53 +0100

    first commit

---
+++ src/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './NavBar.css'
+
+const NavBar = () => {
+
+    return (
+        <nav className="nav">
+            <div className="menu">
+                <div className=""> May Shaw Golf Genius </div>
+            </div>
+        </nav>
+    );
+};
+export default NavBar;
