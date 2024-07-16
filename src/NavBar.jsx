a95eafb894b7ca7842534115e2f04c0228b8de82
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2024-07-16 15:19:59 +0100

    adjusted contents of navbar, styling moved to tailwind classes in .css

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import './NavBar.css'
+import React from "react";
+import "./NavBar.css";
+import clubLogo from "./assets/GolfClubLogo.svg";
 
 const NavBar = () => {
-
+    // {/* <img src={clubLogo} alt="club logo" /> */} need to implement a stylable svg
     return (
-        <nav className="nav">
+        <nav className="navBar">
             <div className="menu">
-                <div className=""> May Shaw Golf Genius </div>
+                <div className="title"> May Shaw Golf Genius </div>
+                <div className="NavItems">
+                    <div className="item"> ******* </div>
+                    <div className="item"> Lessons </div>
+                    <div className="item"> Booking </div>
+                    <div className=""> Contact Me </div>
+                </div>
             </div>
         </nav>
     );
