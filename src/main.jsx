3302bb4ecd0086d78428fae2239744c97e754d55
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2024-07-16 15:18:57 +0100

    formatted

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import NavBar from './NavBar.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import NavBar from "./NavBar.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <NavBar />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
